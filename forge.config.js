b6f3b362963edf230a9b81844f49a7e3ba33d6eb
Adam Cooke <adamcooke@webofwork.com>
2023-07-03 12:37:37 +0100

    first commit

---
+++ forge.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    // osxSign: {},
+    // // ...
+    // osxNotarize: {
+    //   tool: 'notarytool',
+    //   appleId: process.env.APPLE_ID,
+    //   appleIdPassword: process.env.APPLE_PASSWORD,
+    //   teamId: process.env.APPLE_TEAM_ID
+    // }
+    // // ...
+  },
+  publishers: [
+    {
+      name: '@cloudslurp/cloudslurp-desktop',
+      config: {
+        repository: {
+          owner: 'cookiejest',
+          name: 'cloudslurp-desktop'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
